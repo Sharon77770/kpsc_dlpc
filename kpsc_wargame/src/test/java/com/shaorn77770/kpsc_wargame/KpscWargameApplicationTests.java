aa39e7cfc43d4d23e9b7bfeb3459183d3c233fa7
주민재 <john_joo7@naver.com>
2025-05-21 10:45:46 +0900

    create project setting

---
+++ kpsc_wargame/src/test/java/com/shaorn77770/kpsc_wargame/KpscWargameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaorn77770.kpsc_wargame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpscWargameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
