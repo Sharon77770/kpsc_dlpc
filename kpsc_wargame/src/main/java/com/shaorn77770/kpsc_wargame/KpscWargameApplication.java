9150f80009e3c3c761bebef83cb32f9c568a80f7
주민재 <john_joo7@naver.com>
2026-03-10 14:57:30 +0900

    fix init check

--- kpsc_wargame/src/main/java/com/shaorn77770/kpsc_wargame/KpscWargameApplication.java
+++ kpsc_wargame/src/main/java/com/shaorn77770/kpsc_wargame/KpscWargameApplication.java
@@ -13,32 +13,6 @@ public class KpscWargameApplication {
 	private static final Logger logger = LogManager.getLogger(KpscWargameApplication.class);
 
 	public static void main(String[] args) {
-		try {
-            ProcessBuilder pb = new ProcessBuilder("docker", "info");
-            Process process = pb.start();
-
-            int exitCode = process.waitFor();
-            if (exitCode == 0) {
-                logger.info("Docker is running.");
-            } else {
-                logger.error("Docker is not running.");
-				return;
-            }
-        } catch (Exception e) {
-            logger.error("Docker is not installed or not running.", e);
-			return;
-        }
-
-		String host = "localhost";
-        int port = 3306;
-
-        try (Socket socket = new Socket(host, port)) {
-            logger.info("MySQL is running on {}:{}", host, port);
-        } catch (Exception e) {
-            logger.error("MySQL is not running or not reachable.", e);
-			return;
-        }
-
 		SpringApplication.run(KpscWargameApplication.class, args);
 	}
 
